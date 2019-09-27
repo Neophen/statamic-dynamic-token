0b92269edddf0cdfd337c7c33319b1edd3b8c67d
Mykolas Mankevicius <mykolasm@wix.com>
2019-09-27 21:59:49 +0300

    Better documentation

--- DynamicToken/resources/lang/en/settings.php
+++ DynamicToken/resources/lang/en/settings.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'refresh_timer' => 'Refresh time',
-    'refresh_timer_instruct' => "How often after initial load, do you want the csrf token to refresh, in minutes?",
+    'refresh_timer' => 'Refresh interval',
+    'refresh_timer_instruct' => "How often after initial load, do you want the csrf token to refresh? In minutes",
     'selector' => 'CSS Selector',
     'selector_instruct' => "Only change this if you know what you're doing, default `form input[name=\"_token\"]`",
 ];
